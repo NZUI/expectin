4025c00619239067821fa21875cbd49670877b7c
Cressence <cressencey@gmail.com>
2022-06-22 09:28:15 +0100

    complete carousel section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import {
+	Image, Box,
+} from '@chakra-ui/react';
 
 import Header from "./../components/header/header";
 
@@ -6,7 +11,20 @@ const Home = () => {
 	return (
 		<div>
 			<Header />
-			<h1>new header</h1>
+			<Box h={500}>
+				<Carousel>
+					<div>
+						<Image src='https://bit.ly/dan-abramov' alt='Logo' />
+					</div>
+					<div>
+						<Image src='https://bit.ly/dan-abramov' alt='Logo' />
+					</div>
+					<div>
+						<Image src='https://bit.ly/dan-abramov' alt='Logo' />
+					</div>
+				</Carousel>
+			</Box>
+
 		</div>
 	)
 }
