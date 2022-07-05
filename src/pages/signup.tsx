55d3da30539e1bac845403dead949a03f6ba234f
Cressence <cressencey@gmail.com>
2022-07-06 00:00:50 +0100

    complete signup form layout

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -4,19 +4,21 @@ import {
 } from '@chakra-ui/react';
 
 const Signup = () => {
+	const BackgroundImage = require('./../assets/images/auth-image-1.jpeg');
 	return (
-		<Box bg={'gray.50'} h={'calc(100vh)'} pt={'10%'}>
+		<Box bg={'gray.50'} h={'calc(100vh)'} pt={['20%', '20%', '20%', '10%']}>
 			<Container minW={['full', 'full', 'full', 'full', '8xl']}>
 				<Grid
-					mx={'7%'}
+					mx={['1%', '1%', '1%', '7%']}
 					templateRows='repeat(1, 1fr)'
-					templateColumns='repeat(5, 1fr)'
+					templateColumns={['repeat(1, 1fr)', 'repeat(1, 1fr)', 'repeat(1, 1fr)', 'repeat(5, 1fr)']}
 					h={'700px'}
 					boxShadow={'lg'}
 				>
-					<GridItem colSpan={3} roundedTopLeft={'md'} roundedBottomLeft={'md'}>
+					<GridItem colSpan={[1, 1, 1, 3]} roundedTopLeft={'md'} roundedBottomLeft={'md'}>
 						<Box
-							margin={'20'}
+							marginY={'20'}
+							marginX={['3', '3', '20', '20']}
 						>
 							<Text
 								as={'h1'}
@@ -49,12 +51,59 @@ const Signup = () => {
 								<FormLabel htmlFor='email' mb={2}>Confirm Password</FormLabel>
 								<Input placeholder='Password' type='password' borderColor={'gray.300'} />
 							</FormControl>
-							<Button bg={'blue.600'} color={'white'} size='md' w={['full', 'full', 'full', 'full', '50%']} ml={['0', '0', '0', '0', '25%']}>
-								Send
+							<Button
+								bg={'blue.600'}
+								color={'white'}
+								size='md'
+								borderColor={'blue.600'}
+								w={['full', 'full', 'full', 'full', '50%']} ml={['0', '0', '0', '0', '25%']}
+								_hover={{
+									borderWidth: 1,
+									color: 'blue.600',
+									backgroundColor: '#ffffff'
+								}}
+							>
+								Create Account
 							</Button>
 						</Box>
 					</GridItem>
-					<GridItem colSpan={2} bg='papayawhip' roundedTopRight={'md'} roundedBottomRight={'md'}></GridItem>
+					<GridItem
+						colSpan={2}
+						roundedTopRight={'md'}
+						roundedBottomRight={'md'}
+						display={['none', 'none', 'none', 'block']}
+						sx={{
+							background: `url(${BackgroundImage}) center/cover no-repeat`
+						}}
+					>
+						<Box
+							backdropFilter='auto'
+							backdropBrightness={'60%'}
+							h={'100%'}
+							w={'100%'}
+							display={'flex'}
+							justifyContent={'center'}
+							alignItems={'center'}
+						>
+							<Box>
+								<Text
+									color={'white'}
+									as={'h1'}
+									fontSize={'6xl'}
+									textAlign={'center'}
+								>
+									EXPECTIN
+								</Text>
+								<Text
+									color={'white'}
+									fontSize={'md'}
+									textAlign={'center'}
+								>
+									Have this amazing experience with the tools you need.
+								</Text>
+							</Box>
+						</Box>
+					</GridItem>
 				</Grid>
 			</Container>
 
